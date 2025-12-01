fe9910d6f38edbc04aacb9fbefe087b7c053e558
Hugo Benitez <hugo.boca16@gmail.com>
2025-12-01 20:33:32 -0300

    gateway config

--- gateway-service/src/main/java/com/exampleGateway/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/exampleGateway/gateway_service/GatewayServiceApplication.java
@@ -1,16 +1,15 @@
-package com.exampleEureka.eureka_service;
+package com.exampleGateway.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaServiceApplication {
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
